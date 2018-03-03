f68d0ddc84a5a0e90495b76c336ac104ebb92e11
JokoMBI <32702165+JokoMBI@users.noreply.github.com>
2018-03-03 12:13:07 +0100

    Add files via upload

--- LED_Hochbett.ino
+++ LED_Hochbett.ino
@@ -6,7 +6,7 @@
 
 SoftwareSerial BT(10, 11); // RX, TX
 
-byte red, green, blue, bright1 = 255, bright2 = 255, prog;
+byte red, green, blue, bright1 = 255, bright2 = 255, prog = 1;
 bool stripes, spots;
 
 DEFINE_GRADIENT_PALETTE( heatmap_gp ) {
@@ -22,7 +22,7 @@ CRGBPalette256 sunrisePal = heatmap_gp;
 CRGB leds[NUM_LEDS];
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   BT.begin(115200);
   delay(500);
   BT.setTimeout(500);
@@ -45,26 +45,32 @@ void loop() {
     case 5: break;
   }
 
-
   FastLED.show();
 }
 
 
 void readMsg() {
-  char* command;
-  char* dev;
+  String command;
+  String dev;
   char* var;
   byte ivar;
+  char BTMsg[15];
+  int i = 0;
 
-  byte buff[1];
-  //  while (BT.available() > 0) {
-  //    incomingByte = BT.read();
-  //    BTMsg = BTMsg + incomingByte;
-  //  }
+  while (BT.available() > 0 && i < 15) {
+    BTMsg[i] = BT.read();
+    i ++;
+  }
 
-  BT.readBytesUntil(',', command, 5);
-  BT.readBytesUntil(',', dev, 5);
-  BT.readBytesUntil('.', var, 5);
+  //  Serial.println(BTMsg);
+
+  //  BT.readBytesUntil(',', command, 5);
+  //  BT.readBytesUntil(',', dev, 5);
+  //  BT.readBytesUntil('.', var, 5);
+
+  command = strtok(BTMsg, ",.");
+  dev = strtok(NULL, ",.");
+  var = strtok(NULL, ",.");
 
   ivar = atoi(var);
 
@@ -85,10 +91,10 @@ void readMsg() {
     }
   }
 
-  Serial.print(command);
-  Serial.print(dev);
-  Serial.print(var);
-  Serial.print('\n');
+  //  Serial.print(command);
+  //  Serial.print(dev);
+  //  Serial.print(ivar);
+  //  Serial.print('\n');
 
 }
 
@@ -100,19 +106,23 @@ void Sonnenaufgang() {
   static byte sunIndex = 0;
   static byte maxIndex = NUM_LEDS / 4;
   static byte clr = 0;
-  static unsigned int width = 1000;
+  static unsigned int width = 1;
 
   for (byte i = 0; i < NUM_LEDS; i++) {
-    clr = sunIndex * exp((-(i - maxIndex) ^ 2) / (width * 10));
+    clr = sunIndex * exp((-1 * pow((i - maxIndex), 2)) / (width));
     leds[i] = ColorFromPalette(sunrisePal, clr);
   }
 
-  EVERY_N_MILLISECONDS((sunriseLength*60000)/(NUM_LEDS/1/2)) {
+  EVERY_N_MILLISECONDS((sunriseLength * 60000) / (NUM_LEDS / 1 / 2)) {
     if (maxIndex < NUM_LEDS - 1) {
       maxIndex ++;
     }
   }
-
+  EVERY_N_MILLISECONDS(interval) {
+    if (width < 65535) {
+      width ++;
+    }
+  }
   EVERY_N_MILLISECONDS(interval) {
 
     if (sunIndex < 255) {
